7c89ccdbd5f1b4b8240349314b40e8f2901a8208
MoChen <you@example.com>
2023-06-14 16:37:51 +0800

    save

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ import logging
 from module import *
 
 logger = logging.getLogger()
+file_handler = logging.FileHandler('slots_test.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 class slots_test():
@@ -17,10 +22,12 @@ class slots_test():
 
     def run(self):
         for func in self.supported_module:
+            print("{} start test".format(func.module_name))
             logger.info("{} start test".format(func.module_name))
             p = func()
             p.run()
             logger.info("{} test Success".format(func.module_name))
+            print("{} test Success".format(func.module_name))
 
 
 if __name__ == '__main__':
